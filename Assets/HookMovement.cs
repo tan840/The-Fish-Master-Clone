581409d8b569f7e680e1597f01fec444ad380b22
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2021-05-16 12:44:02 +0600

    camera Y movement

--- Assets/HookMovement.cs
+++ Assets/HookMovement.cs
@@ -11,11 +11,13 @@ public class HookMovement : MonoBehaviour
     public bool canMove;
     private float length;
     public float strength;
+    public Collider2D collider;
 
     public Tweener cameraTween; 
     void Awake()
     {
         mainCamera = Camera.main;
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,7 @@ public class HookMovement : MonoBehaviour
             Vector3 pos = transform.position;
             pos.x = mousePos.x;
             transform.position = pos;
+            StartFishing();
         }
         
     }
@@ -35,6 +38,51 @@ public class HookMovement : MonoBehaviour
     {
         length = -50;
         strength = 3;
-        float tiime = (-length) * 0.1f;
+        float time = (-length) * 0.1f;
+        cameraTween = mainCamera.transform.DOMoveY(length, 1+ time * 0.25f, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y <= -11f)
+            {
+                transform.SetParent(mainCamera.transform);
+            }
+            canMove = false;
+        }).OnComplete(delegate
+        {
+
+            collider.enabled = true;
+            cameraTween = mainCamera.transform.DOMoveY(0, time *5f, false).OnUpdate(delegate
+            {
+                if (mainCamera.transform.position.y >= -25f)
+                {
+                    StopFishing();
+                }
+            });
+           
+        });
+
+        //screen game
+        collider.enabled = false;
+        canMove = true;
+
     }    
+
+    void StopFishing()
+    {
+        canMove = false;
+        cameraTween.Kill(false);
+        cameraTween = mainCamera.transform.DOMoveX(0, 2f, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y >= -11f)
+            {
+                transform.SetParent(null);
+                transform.position = new Vector2(transform.position.x, -6);
+            }
+        }).OnComplete(delegate
+        {
+            transform.position = Vector2.down * 6;
+            collider.enabled = false;
+            int num = 0;
+            canMove = true;
+        });
+    }
 }
